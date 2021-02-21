52535df0b954132396bdc819e09b12c2daf28f04
melihbarisik <45101301+melihbarisik@users.noreply.github.com>
2021-02-21 14:56:33 +0300

    HW3 finished

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,35 @@
-#Explain your work
+def prime_first(prime_number):
+    print(prime_number)
 
-#Question 1
-for x in range(a):
-	print(a)
+
+def prime_second(prime_number):
+    print(prime_number)
+
+
+def create_prime_number(num):
+    is_number_prime = False
+
+    if num > 1:
+
+        for i in range(2, num):
+            if (num % i) == 0:
+                is_number_prime = False
+                break
+        else:
+            is_number_prime = True
+
+    else:
+        is_number_prime = False
+
+    return is_number_prime
+
+
+for i in range(1000):
+    if 0 < i < 500:
+        if create_prime_number(i):
+            prime_first(i)
+
+
+    elif 500 < i < 1000:
+        if create_prime_number(i):
+            prime_second(i)
