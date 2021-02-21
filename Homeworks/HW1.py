a85ad679a3141443ea670e37681c2a7687dccbd0
melihbarisik <45101301+melihbarisik@users.noreply.github.com>
2021-02-21 14:54:59 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,35 @@
-#Explain your work
+import random
 
-#Question 1
-for x in range(a):
-	print(a)
+
+def create_prime_number():
+    isPrimeNum = False
+
+    while not isPrimeNum:
+        num = random.randint(1, 9)
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                isPrimeNum = True
+        else:
+            isPrimeNum = False
+
+    return num
+
+
+def print_random_matrix():
+    for i in range(sizeOfMatrix):
+        for j in range(sizeOfMatrix):
+            print(matrix[i][j], end=" ")
+        print()
+
+
+sizeOfMatrix = 3
+matrix = [[0 for i in range(sizeOfMatrix)] for j in range(sizeOfMatrix)]
+
+for i in range(sizeOfMatrix):
+    for j in range(sizeOfMatrix):
+        matrix[i][j] = create_prime_number()
+
+print_random_matrix()
